941add6ef96116f4e02c262d8f93f3e538a88f36
João Costa <costav1402@gmail.com>
2022-07-29 18:52:03 -0300

    feat: add often used types

--- types/index.ts
+++ types/index.ts
@@ -9,8 +9,33 @@ export interface Todo {
   itemcontent: string;
   itemtime: string;
   checked: boolean;
-  subtodo: Array<object>;
   itemid: string;
+  subtodo: Subtodo[];
+}
+export interface Subtodo {
+  itemcontent: string;
+  checked: boolean;
+  subtodoid: string;
 }
 export type Find<T> = (args: T) => boolean;
 export type Delay = (ms: number) => Promise<unknown>;
+export interface UserSession {
+  name: string;
+  email: string;
+  image: string;
+}
+export type RequestBody = {
+  session: {
+    user: UserSession;
+  };
+  token: string;
+};
+export type CollectionID = {
+  collectionid: string;
+};
+export type TodoID = {
+  todoid: string;
+};
+export type SubtodoID = {
+  subtodoid: string;
+};
